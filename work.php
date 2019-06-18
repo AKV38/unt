33fd3f708d8dc2377d3274706b6103ec9c3daf7b
AKV38 <addict.immortal@gmail.com>
2019-06-18 16:39:22 +0800

    edit index and work

--- work.php
+++ work.php
@@ -3,6 +3,7 @@
     <title>Регистрация</title>
 </head>
 <body>
+<h1 align="centre">Reg<?php echo htmlspecialchars($_POST['Reg']); ?></h1>
 <p>Здравствуйте, введите ваши данные для регистрации</p>
 <form action="work2.php" method="post">
     <p>Name: <input type="text" name="name" /></p>
