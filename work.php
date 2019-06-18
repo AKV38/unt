bf359dba80dca665ae7a259969317be3de9220cb
AKV38 <addict.immortal@gmail.com>
2019-06-18 15:39:47 +0800

    create reg

--- work.php
+++ work.php
@@ -1,2 +1,14 @@
-<?php
-echo 'Hi';
+<html>
+<head>
+    <title>Регистрация</title>
+</head>
+<body>
+<p>Здравствуйте, введите ваши данные для регистрации</p>
+<form action="action.php" method="post">
+    <p>Name: <input type="text" name="name" /></p>
+    <p>Email: <input type="email" name="email" /></p>
+    <p>Password: <input type="password" name="password"</p>
+    <p><input type="submit" /></p>
+</form>
+</body>
+</html>
