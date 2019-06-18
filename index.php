5c82bac5fa8d0feb9ed2ad326d72de8878bb5d3d
AKV38 <addict.immortal@gmail.com>
2019-06-18 16:32:46 +0800

    creat index

---
+++ index.php
@@ -0,0 +1,18 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+    <title></title>
+</head>
+<style>
+    .main {
+        background: violet;
+
+    }
+</style>
+<body>
+<div class ="main">
+    <form action="work.php" method="post">
+        <p><input type="submit" name="Регистрация" /></p>
+    </form>
+</div>
+<p>Бла бла бла</p>
+</body>
